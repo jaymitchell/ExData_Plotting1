035a4076030d609ab3c4ad2b201fa6071e18134e
Jay Mitchell <jaybmitchell@gmail.com>
2014-06-08 18:13:04 -0600

    Finished plot 2

--- plot2.R
+++ plot2.R
@@ -9,9 +9,13 @@ data_as_date = as.Date(all_data$Date, format="%d/%m/%Y")
 data_to_plot <- subset(all_data, data_as_date==as.Date("2007-02-01") |
                                  data_as_date==as.Date("2007-02-02"))
 
+# Create datetime to plot
+datetime <- paste(data_to_plot$Date, data_to_plot$Time, sep = ' ')
+datetime <- strptime(datetime, format = '%d/%m/%Y %H:%M:%S')
+
 # Plot data
 
 png("plot2.png", width=480, height=480, units="px")
-plot(data_to_plot$Time, data_to_plot$Global_active_power,
-     ylab="Global Active Power (kilowatts)")
+plot(datetime, data_to_plot$Global_active_power,
+     ylab="Global Active Power (kilowatts)", xlab="", type="l")
 dev.off()
